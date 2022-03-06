56585b33ef5db808037190daad7a2d8dca2f08ff
GianniPio <giannipiopellegrino00@gmail.com>
2022-03-06 17:40:01 +0100

    color form

--- resources/views/pages/register.blade.php
+++ resources/views/pages/register.blade.php
@@ -13,27 +13,29 @@
     @endif
 
     {{-- form di registrazione --}}
-    <form class="form" action="{{route('register')}}" method="POST">
+    <div id="containerForm" class="mx-auto my-5">
+        <form class="form mx-auto" action="{{route('register')}}" method="POST">
 
-        @method('POST')
-        @csrf
+            @method('POST')
+            @csrf
 
-        <label for="name">Nome</label>
-        <input type="text" name="name">
-        <label for="surname">Cognome</label>
-        <input type="text" name="surname">
-        <label for="email">Email</label>
-        <input type="email" name="email">
-        <label for="password">Password</label>
-        <input type="password" name="password">
-        <label for="password_confirmation">Password-confirmation</label>
-        <input type="password" name="password_confirmation">
-        <label for="birthday">Data di nascita</label>
-        <input type="date" name="birthday">
-        <label for="description">Descrizione</label>
-        <input type="textarea" name="description">
+            <label for="name">Nome</label>
+            <input type="text" name="name">
+            <label for="surname">Cognome</label>
+            <input type="text" name="surname">
+            <label for="email">Email</label>
+            <input type="email" name="email">
+            <label for="password">Password</label>
+            <input type="password" name="password">
+            <label for="password_confirmation">Password-confirmation</label>
+            <input type="password" name="password_confirmation">
+            <label for="birthday">Data di nascita</label>
+            <input type="date" name="birthday">
+            <label for="description">Descrizione</label>
+            <input type="textarea" name="description">
 
-        <input class='my-4 btn btn-primary' type="submit" value= "Register">
-    </form>
+            <input class='my-4 btn' type="submit" value= "Register">
+        </form>
+    </div>
     
 @endsection
